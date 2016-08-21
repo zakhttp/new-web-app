dddd4bc32f2629144499ce7b8a7fe7b997954651
zakhttp <zakhttp@gmail.com>
2016-08-21 22:56:49 +0400

    Create directory init script

---
+++ nwa.sh
@@ -0,0 +1,17 @@
+echo "Creating images folder";
+mkdir images;
+echo "Creating js folder";
+mkdir js;
+echo "creating app.js file"
+mkdir js/app.js;
+echo "Creating css folder";
+mkdir css;
+echo "Creating master.css file";
+touch css/master.css;
+echo "Creating index.html";
+touch index.html;
+echo "Creating .gitignore file";
+echo ".DS_Store" >> .gitignore;
+echo "Intitilizing the git repository";
+git init;
+
